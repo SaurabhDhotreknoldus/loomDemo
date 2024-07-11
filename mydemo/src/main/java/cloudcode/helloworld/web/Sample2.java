f00afa14a5fc73bf938b0591540ee6ad065af818
kuldeep <kuldeep.swaroop@nashtechglobal.com>
2024-07-11 16:41:28 +0530

    Added sample controller

---
+++ mydemo/src/main/java/cloudcode/helloworld/web/Sample2.java
@@ -0,0 +1,15 @@
+package cloudcode.helloworld.web;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/loom")
+public class Sample2 {
+
+    @GetMapping("/sample2")
+    public ResponseEntity<String> searchPosts() {
+        return ResponseEntity.ok("Ok Tested Sample 2");
+    }
+}
